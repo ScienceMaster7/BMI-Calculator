d1f1b09c3008aa3decaabf33f6e0b108842450fe
Benedikt Walch <benx.bw@gmail.com>
2021-07-06 07:29:36 +0200

    Fix of if else statements 2 and 3

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ formElement.addEventListener("submit", (event) => {
   let weight = formElement.weight.value;
   let bmi = weight / height ** 2;
 
-  pElement.textContent = "Your Body Mass Index is " + bmi;
+  pElement.textContent = "Your Body Mass Index is " + bmi.toFixed(2);
 
   if (bmi < 18.5) {
     pElement.style.color = "red";
-  } else if (18.5 < bmi < 25) {
+    console.log(1);
+  } else if (18.5 < bmi && bmi < 25) {
     pElement.style.color = "green";
-  } else if (25 < bmi < 30) {
+    console.log(2);
+  } else if (25 < bmi && bmi < 30) {
     pElement.style.color = "orange";
+    console.log(3);
   } else {
     pElement.style.color = "red";
+    console.log(4);
   }
 
   formElement.reset();
