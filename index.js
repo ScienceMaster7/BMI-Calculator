7615eb4e037994c6dd0d00e5796807704bae9912
Benedikt Walch <benx.bw@gmail.com>
2021-07-06 08:30:30 +0200

    Values are displayed as 1 item below the calculator, clicking on the delete button of an item deletes this item

--- index.js
+++ index.js
@@ -8,26 +8,54 @@ mainElement.append(divContainer);
 
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
-  let height = formElement.height.value;
-  let weight = formElement.weight.value;
-  let bmi = weight / height ** 2;
-  let name = formElement.name.value;
+  const height = formElement.height.value;
+  const weight = formElement.weight.value;
+  const bmi = weight / height ** 2;
+  const name = formElement.name.value;
 
-  pElement.textContent = "Your Body Mass Index is " + bmi.toFixed(2);
+  const listItem = document.createElement("div");
+  listItem.classList.add("container__item");
+
+  const p1 = document.createElement("p");
+  const p2 = document.createElement("p");
+  const p3 = document.createElement("p");
+  const deleteItem = document.createElement("button");
+
+  p1.textContent = name;
+  p2.textContent = bmi.toFixed(2);
 
   if (bmi < 18.5) {
-    pElement.style.color = "red";
+    p3.textContent = "under-weight";
+    p3.style.color = "red";
     console.log(1);
   } else if (18.5 < bmi && bmi < 25) {
-    pElement.style.color = "green";
+    p3.textContent = "normal";
+    p3.style.color = "green";
     console.log(2);
   } else if (25 < bmi && bmi < 30) {
-    pElement.style.color = "orange";
+    p3.textContent = "obese";
+    p3.style.color = "orange";
     console.log(3);
   } else {
-    pElement.style.color = "red";
+    p3.textContent = "over-weight";
+    p3.style.color = "red";
     console.log(4);
   }
+
+  deleteItem.textContent = "delete";
+  deleteItem.style.backgroundColor = "red";
+  deleteItem.style.fontStyle = "bold";
+
+  listItem.append(p1);
+  listItem.append(p2);
+  listItem.append(p3);
+  listItem.append(deleteItem);
+
+  divContainer.append(listItem);
+
+  listItem.addEventListener("click", (event) => {
+    listItem.remove(listItem);
+  });
 
   formElement.reset();
 });
