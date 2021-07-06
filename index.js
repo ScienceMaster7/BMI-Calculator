e8c04a9f63b74665a084ba9e836c8c12e50dc753
Benedikt Walch <benx.bw@gmail.com>
2021-07-06 08:51:39 +0200

    Addition of comments to explain what the code does

--- index.js
+++ index.js
@@ -9,25 +9,32 @@ mainElement.append(divContainer);
 let counter = 0;
 
 formElement.addEventListener("submit", (event) => {
-  counter++;
-  console.log(counter);
   event.preventDefault();
+
+  counter++;
+
+  //variables that store the value of the input.
   const height = formElement.height.value;
   const weight = formElement.weight.value;
   const bmi = weight / height ** 2;
   const name = formElement.name.value;
 
+  //creating a new list Item.
   const listItem = document.createElement("div");
   listItem.classList.add("container__item");
 
+  // creating 3 paragraphs and button.
   const p1 = document.createElement("p");
   const p2 = document.createElement("p");
   const p3 = document.createElement("p");
   const deleteItem = document.createElement("button");
 
+  // asigning paragraph 1 and 2 their coresponding values from the input.
   p1.textContent = name;
   p2.textContent = bmi.toFixed(2);
 
+  /* Conditional test to check if a person is underweight, normal, obese or 
+  overweight and setting the coresponding text color. */
   if (bmi < 18.5) {
     p3.textContent = "under-weight";
     p3.style.color = "red";
@@ -42,20 +49,25 @@ formElement.addEventListener("submit", (event) => {
     p3.style.color = "red";
   }
 
+  // Styling the delete Button.
   deleteItem.textContent = "delete";
   deleteItem.style.backgroundColor = "red";
   deleteItem.style.fontStyle = "bold";
 
+  //appending the 3 new paragraphs and 1 button to the new list item.
   listItem.append(p1);
   listItem.append(p2);
   listItem.append(p3);
   listItem.append(deleteItem);
 
+  // appending the list item to the parent container.
   divContainer.append(listItem);
 
+  // If the delete button is clicked this list item is removed.
   listItem.addEventListener("click", (event) => {
     listItem.remove(listItem);
   });
 
+  // reseting the form if the Calculate button is clicked.
   formElement.reset();
 });
